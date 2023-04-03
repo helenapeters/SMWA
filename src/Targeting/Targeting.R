e529587c0b671de751e43baa5fa6d241d60fd548
Helena Peters <94479329+helenapeters@users.noreply.github.com>
2023-04-03 19:09:27 +0200

    Update Targeting.R

--- src/Targeting/Targeting.R
+++ src/Targeting/Targeting.R
@@ -20,14 +20,14 @@ setwd("C:/Users/Helena/Documents/eerste_master/SMWA/Project_test/data_scrape")
 ## Only need to run this once
 #auth_setup()
 
-# Look up the account for Orvaline and look at the best match
+## Look up the account for Orvaline and look at the best match
 my_name <- "Orvaline"
 orvaline <- search_accounts(my_name)[1, ]
 
-# This results in a account object with all kinds of information
+## This results in a account object with all kinds of information
 glimpse(orvaline)
 
-# Retrieve all her followers
+## Retrieve all her followers
 firstdegree <- get_account_followers(
   orvaline$id,
   limit = orvaline$followers_count,
@@ -43,10 +43,10 @@ for (i in 1:length(firstdegree)) ##nrow instead of length?
   seconddegree[[i]] <- search_accounts(firstdegree$username[i])[1, ]
 seconddegree <- bind_rows(seconddegree)
 
-#this should be 374:
+## This should be 374:
 nrow(seconddegree)  ##not correct!
 
-# Now extract the followers-of-followers
+## Now extract the followers-of-followers
 seconddegreefollowers <- list()
 for (i in 1:nrow(seconddegree)) {
   cat('... Scraping: ', seconddegree$username[i], '\n')
@@ -55,9 +55,72 @@ for (i in 1:nrow(seconddegree)) {
   )
 }
 
+## Now we have all the followers of followers
+## Let's add the first degree followers to that list
+seconddegreefollowers[[length(seconddegreefollowers)+1]] <- firstdegree
+
+## Let's extract all the usernames of the followers
+followers <- list()
+for (i in 1:length(seconddegreefollowers))
+  followers[[i]] <- seconddegreefollowers[[i]] %>% pull(username)  #Fout bij username
+names(followers) <- c(seconddegree$username,orvaline$username)
+
+## Let's have a look
+glimpse(followers)
+
+## Transform that list to a character vector of length 5
+## Each element in the vector contains all the followers of a user
+
+mm <- do.call("c", lapply(followers, paste, collapse=" "))
+
 #####################################
 ## Step 3: create adjacency matrix ##
 #####################################
+
+## Install and load the text mining package to preprocess the data
+p_load(SnowballC, tm, igraph)
+
+## Transform that vector using the tm package to structure the unstructured data
+myCorpus <- Corpus(VectorSource(mm))
+
+## Inspect the result
+inspect(myCorpus)
+
+## This creates a matrix, in which the rows are our sources of interest and the columns are the friends
+## This thus resembles an incidence matrix
+userfollower <- DocumentTermMatrix(myCorpus, control = list(wordLengths = c(0, Inf)))
+
+## We can also look at the actual matrix
+inspect(userfollower)
+
+## Compute the adjacency matrix using matrix multiplication.
+A <- t(as.matrix(userfollower)) %*% as.matrix(userfollower)
+
+## Look at its dimensions
+dim(A)
+
+## Create a graph object based on the adjacency matrix & remove loop edges
+g <- graph.adjacency(A, weighted=TRUE,
+                     mode ='undirected') %>% simplify()
+
+## Set labels and degrees of vertices
+V(g)$label <- V(g)$name
+V(g)$degree <- igraph::degree(g)
+## Plot the Graph
+set.seed(3952)
+## Prepare graph
+layout <- layout.auto(g)
+## Give the graph lots of room
+
+mar <- par()$mar ## Store this for later
+par(mar=rep(0, 4))
+plot(g, layout=layout, vertex.label=NA,
+     edge.curved=TRUE,vertex.size=3,
+     vertex.color=c("green","red","blue")[ifelse(V(g)$name %in%
+                                                   names(igraph::degree(g)[tail(order(igraph::degree(g)),5)]) ==TRUE,1,
+                                                 ifelse(V(g)$name %in%
+                                                          names(igraph::degree(g)[tail(order(igraph::betweenness(g)),10)]) ==TRUE,2,3))])
+
 
 ##############################################
 ## Step 4: compute degree for all followers ##
