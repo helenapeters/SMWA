1cfb9a28cfc0afb5fcb8d429bc779843b9c8bee6
Helena Peters <94479329+helenapeters@users.noreply.github.com>
2023-04-04 16:00:50 +0200

    Update Targeting.R

--- src/Targeting/Targeting.R
+++ src/Targeting/Targeting.R
@@ -27,15 +27,15 @@ orvaline <- search_accounts(my_name)[1, ]
 ## This results in a account object with all kinds of information
 glimpse(orvaline)
 
-## Retrieve all her followers
-firstdegree <- get_account_followers(
-  orvaline$id,
-  limit = orvaline$followers_count,
-  retryonratelimit = TRUE
-)
-
-## Save the data as "firstdegree"
-save(firstdegree, file = "firstdegree.RData")
+# ## Retrieve all her followers
+# firstdegree <- get_account_followers(
+#   orvaline$id,
+#   limit = orvaline$followers_count,
+#   retryonratelimit = TRUE
+# )
+# 
+# ## Save the data as "firstdegree"
+# save(firstdegree, file = "firstdegree.RData")
 
 ## Load the data by using the command:
 load("firstdegree.RData")
@@ -47,21 +47,21 @@ firstdegree
 ## Step 2: get followers of followers ##
 ########################################
 
-## Get followers of followers
-seconddegreefollowers <- list()
-for (i in 1:nrow(firstdegree)) {
-  cat('... Scraping: ', firstdegree$username[i], '\n')
-  seconddegreefollowers[[i]] <- get_account_followers(
-    firstdegree$id[i], limit = firstdegree$followers_count[i], retryonratelimit = TRUE
-  )
-}
-
-## Now we have all the followers of followers
-## Let's add the first degree followers to that list
-seconddegreefollowers[[length(seconddegreefollowers)+1]] <- firstdegree
-
-## Save the data as "network"
-rlist::list.save(seconddegreefollowers, file = "network.RData") # It takes too long to scrape everytime
+# ## Get followers of followers
+# seconddegreefollowers <- list()
+# for (i in 1:nrow(firstdegree)) {
+#   cat('... Scraping: ', firstdegree$username[i], '\n')
+#   seconddegreefollowers[[i]] <- get_account_followers(
+#     firstdegree$id[i], limit = firstdegree$followers_count[i], retryonratelimit = TRUE
+#   )
+# }
+# 
+# ## Now we have all the followers of followers
+# ## Let's add the first degree followers to that list
+# seconddegreefollowers[[length(seconddegreefollowers)+1]] <- firstdegree
+# 
+# ## Save the data as "network"
+# rlist::list.save(seconddegreefollowers, file = "network.RData") # It takes too long to scrape everytime
 
 ## Load the data by using the command:
 seconddegreefollowers <- rlist::list.load("network.RData")
@@ -91,20 +91,20 @@ userfollower <- DocumentTermMatrix(myCorpus, control = list(wordLengths = c(0, I
 ## We can also look at the actual matrix
 inspect(userfollower)   ##problem here length 6
 
-## Create an adjacency matrix with 1/0 links between rows
-adj_matrix <- matrix(nrow = nrow(userfollower), ncol = nrow(userfollower))
-
-## Set the elements of the adjacency matrix based on whether the column user follows the row user
-for (i in 1:nrow(adj_matrix)) {
-  row_user <- names(userfollower)[i]
-  for (j in 1:ncol(adj_matrix)) {
-    col_user <- names(userfollower)[j]
-    adj_matrix[i, j] <- as.numeric(col_user %in% userfollower[[row_user]])
-  }
-}
-
-## Save the adjecency matrix as "adj_matrix"
-save(adj_matrix, file = "adj_matrix.RData")
+# ## Create an adjacency matrix with 1/0 links between rows
+# adj_matrix <- matrix(nrow = nrow(userfollower), ncol = nrow(userfollower))
+# 
+# ## Set the elements of the adjacency matrix based on whether the column user follows the row user
+# for (i in 1:nrow(adj_matrix)) {
+#   row_user <- names(userfollower)[i]
+#   for (j in 1:ncol(adj_matrix)) {
+#     col_user <- names(userfollower)[j]
+#     adj_matrix[i, j] <- as.numeric(col_user %in% userfollower[[row_user]])
+#   }
+# }
+# 
+# ## Save the adjecency matrix as "adj_matrix"
+# save(adj_matrix, file = "adj_matrix.RData")
 
 ## Load the data by using the command:
 load("adj_matrix.RData")
