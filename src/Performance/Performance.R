42c239440845fce94b906315236910ccedaafc0b
Helena Peters <94479329+helenapeters@users.noreply.github.com>
2023-04-04 17:36:26 +0200

    Add files via upload

---
+++ src/Performance/Performance.R
@@ -0,0 +1,63 @@
+###################################################################
+####################### PART 1: DESCRIPTIVE #######################
+###################################################################
+
+## Clean the environment
+rm(list=ls()) 
+
+## Install and load packages
+if (!require("pacman")) install.packages("pacman", quiet=TRUE) ; require("pacman")
+p_load(rtoot, httpuv, tidyverse, lubridate, rvest)
+
+## Set working directory
+setwd("C:/Users/Helena/Documents/eerste_master/SMWA/Project_test/data_scrape")
+
+######################
+## Step 1: Scraping ##
+######################
+
+##############
+## Mastodon ##
+##############
+
+## Set up authentication
+## Only need to run this once
+#auth_setup()
+
+## Search for 50 toots using the amelielens hashtag
+toots <- get_timeline_hashtag("#amelielens", limit = 50, retryonratelimit = TRUE) ##Only 6?
+
+## Preview toot data
+glimpse(toots)
+
+############
+## Reddit ##
+############
+
+#############
+## Spotify ##
+#############
+
+#############
+##  ...    ##
+#############
+
+########################
+## Step 2: Wordclouds ##
+########################
+
+########################
+## Step 3: Wordgraphs ##
+########################
+
+#############################
+## Step 4: Topic modelling ##
+#############################
+
+################################
+## Step 5: Sentiment analysis ##
+################################
+
+##################################################################
+####################### PART 2: PREDICTIVE #######################
+##################################################################
