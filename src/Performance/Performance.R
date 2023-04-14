a50edd0b3e9cfc6f46603d6b7d3a8ebdd57f0b8e
Helena Peters <94479329+helenapeters@users.noreply.github.com>
2023-04-14 16:09:47 +0200

    Update Performance.R

--- src/Performance/Performance.R
+++ src/Performance/Performance.R
@@ -7,7 +7,7 @@ rm(list=ls())
 
 ## Install and load packages
 if (!require("pacman")) install.packages("pacman", quiet=TRUE) ; require("pacman")
-p_load(rtoot, httpuv, tidyverse, lubridate, rvest, caret)
+p_load(rtoot, httpuv, tidyverse, lubridate, rvest, caret, dplyr)
 
 ## Set working directory
 setwd("C:/Users/Helena/Documents/eerste_master/SMWA/Project_test/data_scrape")
@@ -25,7 +25,7 @@ setwd("C:/Users/Helena/Documents/eerste_master/SMWA/Project_test/data_scrape")
 #auth_setup()
 
 ## Search for 50 toots using the amelielens hashtag
-toots <- get_timeline_hashtag("#amelielens", limit = 50, retryonratelimit = TRUE) ##Only 6?
+toots <- get_timeline_hashtag("#metallica", limit = 50, retryonratelimit = TRUE) ##Only 6?
 
 ## Preview toot data
 glimpse(toots)
@@ -78,33 +78,130 @@ glimpse(toots)
 ####################### PART 2: PREDICTIVE #######################
 ##################################################################
 
+################## Read data ####################
+
+# ## Save the data as "metallica2"
+# save(metallica2, file = "metallica2.RData")
+# 
+# ## Save the data as "metallica3"
+# save(metallica3, file = "metallica3.RData")
+
+## Load the data by using the command:
+load("metallica2.RData")
+
+## Load the data by using the command:
+load("metallica3.RData")
+
+################## Clean data (1) ####################
+
+## Delete duplicates
+metallica_no_popularity <- metallica2[!duplicated(metallica2$track_name), ]
+
+## Change the name of track.id column
+names(metallica3)[names(metallica3) == "track.id"] <- "track_id"
+
+## Create the full dataframe
+metallica_full <- merge(x = metallica_no_popularity, y = metallica3, by= 'track_id')
+
+## Create Basetable
+train_val_basetable <- metallica_full
+
+################## Split data ####################
+
+#####Still need to add test set!!!!!!!!!!
+
+#A seed is set for reproducing purposes
+set.seed(1)
+
+split_sample <- sample(nrow(train_val_basetable), 0.8*nrow(train_val_basetable))
+
+training <- train_val_basetable[split_sample, ]
+validation <- train_val_basetable[-split_sample, ]
+
+#separate dependent and independent variables
+training_X <- subset(training, select = -c(track.popularity))
+training_y <- training$track.popularity
+
+validation_X <- subset(validation, select = -c(track.popularity))
+validation_y <- validation$track.popularity
+
+str(training_X)
+str(training_y)
+
+################## Clean data (2) ####################
+
+## Take subset --Mss trackid verwijderen aangezien we al trackname hebben
+training_X <- subset(training_X, select = c(track_id, album_release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms, track_name, track_number, album_name, mode_name, key_mode, added_at, explicit))
+validation_X <- subset(validation_X, select = c(track_id, album_release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms, track_name, track_number, album_name, mode_name, key_mode, added_at, explicit))
+
+# ## Parsing dates   ----------Klopt nog niet-------------
+# 
+# # Save your current locale
+# original_locale <- Sys.getlocale(category = "LC_TIME")
+# # Change it to US
+# Sys.setlocale(category = "LC_TIME", locale = "English_United States")
+# 
+# # Transform the album_release_date into a posixct format for further manipulations
+# training_X$album_release_date <- as.POSIXct(training_X$album_release_date, format = "%B %e %Y")
+# validation_X$album_release_date <- as.POSIXct(validation_X$album_release_date, format = "%B %e %Y")
+# #test_X$album_release_date <- as.POSIXct(test_X$album_release_date, format = "%B %e %Y")
+# 
+# # Transform the added_at into a posixct format for further manipulations
+# training_X$added_at <- as.POSIXct(training_X$added_at, format = "%B %e %Y")
+# validation_X$added_at <- as.POSIXct(validation_X$added_at, format = "%B %e %Y")
+# #test_X$added_at <- as.POSIXct(test_X$added_at, format = "%B %e %Y")
+# 
+# # Change back to the original locale
+# Sys.setlocale(category = "LC_TIME", locale = original_locale)
+
 #################################
 ## Step 1: Feature engineering ##
 #################################
 
-#########################################
-## Step 2: Text and sentiment analysis ##
-#########################################
+###################### Encoding of categorical data ######################
+
+## One-hot encoding
+
+# Here are the categorical variables
+cols_to_encode <- c("album_name", "mode_name", "key_mode", "explicit")
+
+# We retain all the possible categories
+cats <- categories(training_X[, cols_to_encode], p = c(4,2,9,2))
+
+# apply on train set and exclude reference categories  ----- E major zal een andere naam moeten krijgen en FALSE klop wss ook niet
+dummy_train <- dummy(training_X[, cols_to_encode], object = cats)
+dummy_train <- subset(dummy_train, select = -c(Load, major, E major, FALSE))
+# apply on validation set and exclude reference categories
+dummy_val <- dummy(validation_X[, cols_to_encode], object = cats)
+dummy_val <- subset(dummy_val, select = -c(Load, major, E major, FALSE))
+# apply on test set and exclude reference categories
+dummy_test <- dummy(test_X[, cols_to_encode], object = cats)
+dummy_test <- subset(Load, major, E major, FALSE))
+
+# merge with overall training set
+training_X <- subset(training_X, select = -c(album_name, mode_name, key_mode, explicit))
+training_X <- cbind(training_X, dummy_train)
+# merge with overall validation set
+validation_X <- subset(validation_X, select = -c(album_name, mode_name, key_mode, explicit))
+validation_X <- cbind(validation_X, dummy_val)
+# merge with overall test set
+test_X <- subset(test_X, select = -c(album_name, mode_name, key_mode, explicit))
+test_X <- cbind(test_X, dummy_test)
+
 
 #####################
-## Step 3: Predict ##
+## Step 2: Predict ##
 #####################
 
-## Read data
-
-## Normalize the data
-
-## Data splitting
-
-## Basetable
-
-train_val_basetable <- 
 
 ##################
 ## Linear model ##
 ##################
 
-lm_model <- lm(sentiment ~., data = train_val_basetable)
+## Cleaning
+training_X <- subset(training_X, select = c(track.duration_ms, track.name, track.track_number, track.album.name, track.album.release_date, track.album.total_tracks))
+validation_X <- subset(validation_X, select = c(track.duration_ms, track.name, track.track_number, track.album.name, track.album.release_date, track.album.total_tracks))
+lm_model <- lm(track.popularity ~., data = train_val_basetable)
 predictions_lm <- predict(lm_model, X_test)
 mae_lm <- sum(abs(predictions - Y_test))/nrow(X_test) * std[[10]]
 mae_lm
