adae942ecd0ef2f50086170cac803eac39fe60e4
iriscappoen <94065573+iriscappoen@users.noreply.github.com>
2023-04-15 11:56:44 +0200

    Update DC and FE

--- src/Performance/Performance.R
+++ src/Performance/Performance.R
@@ -10,7 +10,7 @@ if (!require("pacman")) install.packages("pacman", quiet=TRUE) ; require("pacman
 p_load(rtoot, httpuv, tidyverse, lubridate, rvest, caret, dplyr)
 
 ## Set working directory
-setwd("C:/Users/Helena/Documents/eerste_master/SMWA/Project_test/data_scrape")
+setwd("C:/Users/irisc/OneDrive/Documenten/GitHub/SMWA/src/Performance")
 
 ######################
 ## Step 1: Scraping ##
@@ -95,24 +95,27 @@ is_binary <- function(v) {
 # save(metallica3, file = "metallica3.RData")
 
 ## Load the data by using the command:
-load("metallica2.RData")
+#load("metallica2.RData")
 
 ## Load the data by using the command:
-load("metallica3.RData")
+#load("metallica3.RData")
+
+## Load the data by using the command:
+load("metallicafull.RData")
 
 ################## Merge data ####################
 
-## Delete duplicates
-metallica_no_popularity <- metallica2[!duplicated(metallica2$track_name), ]
+### Delete duplicates
+#metallica_no_popularity <- metallica2[!duplicated(metallica2$track_name), ]
 
 ## Change the name of track.id column
-names(metallica3)[names(metallica3) == "track.id"] <- "track_id"
+#names(metallica3)[names(metallica3) == "track.id"] <- "track_id"
 
 ## Create the full dataframe
-metallica_full <- merge(x = metallica_no_popularity, y = metallica3, by= 'track_id')
+#metallica_full <- merge(x = metallica_no_popularity, y = metallica3, by= 'track_id')
 
 ## Create Basetable
-train_val_basetable <- metallica_full
+train_val_basetable <- Metallicafull
 
 ################## Exploratory data analysis ####################
 
@@ -122,18 +125,19 @@ check_cols <- sapply(train_val_basetable, is.numeric)
 
 numeric_cols <- colnames(train_val_basetable[,check_cols])
 numeric_cols
-# the data contains 21 numeric columns
+# the data contains 17 numeric columns
 
 category_cols <- colnames(train_val_basetable[,!check_cols])
 category_cols
-#the data contains 59 categorical columns
+#the data contains 41 categorical columns
 
 ## Numeric columns ---------------------------------------------------------
 
 # Check if there are binary columns
 binary_cols<-vapply(train_val_basetable[,numeric_cols], is_binary, logical(1))
 binary_cols
-#we can see that there are no binary columns.
+#only mode is a binary variable
+# 1 represents major, 0 represents minor
 
 ### Visual exploration
 
@@ -196,15 +200,16 @@ str(training_y)
 ################## Clean data ####################
 
 ## Take subset -- Mss trackid nog verwijderen aangezien we al trackname hebben
-training_X <- subset(training_X, select = c(track_id, album_release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms, track_name, track_number, album_name, mode_name, key_mode, added_at, explicit))
-validation_X <- subset(validation_X, select = c(track_id, album_release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms, track_name, track_number, album_name, mode_name, key_mode, added_at, explicit))
+training_X <- subset(training_X, select = c(track.id, track.album.release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, track.duration_ms, track.name, track.track_number))
+validation_X <- subset(validation_X, select = c(track.id, track.album.release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, track.duration_ms, track.name, track.track_number))
+
 
 # Multiply the duration column to get duration in minutes
-training_X$duration_ms <- training_X$duration_ms * 0.000016666666666666667
-validation_X$duration_ms <- validation_X$duration_ms * 0.000016666666666666667
+training_X$track.duration_ms <- training_X$track.duration_ms * 0.000016666666666666667
+validation_X$track.duration_ms <- validation_X$track.duration_ms * 0.000016666666666666667
 ## Change the name of duration column
-names(training_X)[names(training_X) == "duration_ms"] <- "duration_min"
-names(validation_X)[names(validation_X) == "duration_ms"] <- "duration_min"
+names(training_X)[names(training_X) == "track.duration_ms"] <- "duration_min"
+names(validation_X)[names(validation_X) == "track.duration_ms"] <- "duration_min"
 
 
 #----------------Tot hier runt het-----------------------------------------------------
@@ -223,11 +228,6 @@ training_X$album_release_date <- as.POSIXct(training_X$album_release_date, forma
 validation_X$album_release_date <- as.POSIXct(validation_X$album_release_date, format = "%B %e %Y")
 #test_X$album_release_date <- as.POSIXct(test_X$album_release_date, format = "%B %e %Y")
 
-# Transform the added_at into a posixct format for further manipulations
-training_X$added_at <- as.POSIXct(training_X$added_at, format = "%B %e %Y")
-validation_X$added_at <- as.POSIXct(validation_X$added_at, format = "%B %e %Y")
-#test_X$added_at <- as.POSIXct(test_X$added_at, format = "%B %e %Y")
-
 # Change back to the original locale
 Sys.setlocale(category = "LC_TIME", locale = original_locale)
 
@@ -238,29 +238,39 @@ Sys.setlocale(category = "LC_TIME", locale = original_locale)
 ###################### Encoding of categorical data ######################
 
 ## One-hot encoding
+library(dummy)
+
+#
+names(training_X)[names(training_X) == "mode"] <- "mode_major"
+names(validation_X)[names(validation_X) == "mode"] <- "mode_major"
+training_X$mode_major <- as.character(training_X$mode_major)
+validation_X$mode_major <- as.character(validation_X$mode_major)
 
 # Here are the categorical variables
-cols_to_encode <- c("album_name", "mode_name", "key_mode", "explicit")
+#cols_to_encode <- c("key")
+
+
 
 # We retain all the possible categories
-cats <- categories(training_X[, cols_to_encode], p = c(4,2,9,2))
+training_X$key <- as.character(training_X$key)
+cats <- categories(training_X["key"])
 
-# apply on train set and exclude reference categories  ----- E major zal een andere naam moeten krijgen en FALSE klop wss ook niet
-dummy_train <- dummy(training_X[, cols_to_encode], object = cats)
-dummy_train <- subset(dummy_train, select = -c(Load, major, E major, FALSE))
+# apply on train set and exclude reference categories  ----- reference category: album Load and key 4 (E)
+dummy_train <- dummy(training_X["key"], object = cats)
+dummy_train <- subset(dummy_train, select = -c(key_4))
 # apply on validation set and exclude reference categories
-dummy_val <- dummy(validation_X[, cols_to_encode], object = cats)
-dummy_val <- subset(dummy_val, select = -c(Load, major, E major, FALSE))
+dummy_val <- dummy(validation_X["key"], object = cats)
+dummy_val <- subset(dummy_val, select = -c(key_4))
 # apply on test set and exclude reference categories
 #dummy_test <- dummy(test_X[, cols_to_encode], object = cats)
 #dummy_test <- subset(Load, major, E major, FALSE))
 
 # merge with overall training set
-training_X <- subset(training_X, select = -c(album_name, mode_name, key_mode, explicit))
-training_X <- cbind(training_X, dummy_train)
+training_X_encode <- subset(training_X, select = -c(key))
+training_X_encode <- cbind(training_X_encode, dummy_train)
 # merge with overall validation set
-validation_X <- subset(validation_X, select = -c(album_name, mode_name, key_mode, explicit))
-validation_X <- cbind(validation_X, dummy_val)
+validation_X_encode <- subset(validation_X, select = -c(key))
+validation_X_encode <- cbind(validation_X_encode, dummy_val)
 # merge with overall test set
 #test_X <- subset(test_X, select = -c(album_name, mode_name, key_mode, explicit))
 #test_X <- cbind(test_X, dummy_test)
@@ -274,10 +284,12 @@ validation_X <- cbind(validation_X, dummy_val)
 ##################
 ## Linear model ##
 ##################
+train_X <- subset(training_X_encode, select = -c(track.id, track.name))
+val_X <- subset(validation_X_encode, select = -c(track.id, track.name))
 
-lm_model <- lm(training_y ~., data = training_X)
-predictions_lm <- predict(lm_model, validation_X)
-mae_lm <- sum(abs(predictions - validation_y))/nrow(validation_X) * std[[10]]
+lm_model <- lm(training_y ~., data = train_X)
+predictions_lm <- predict(lm_model, val_X)
+mae_lm <- sum(abs(predictions_lm - validation_y))/nrow(val_X) * std[[10]]
 mae_lm
 
 ################
